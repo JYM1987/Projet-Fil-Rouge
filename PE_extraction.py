b4c30c706cb23a0dacab80121f11e1080b72d87d
JYM1987 <146350252+JYM1987@users.noreply.github.com>
2023-12-10 19:09:35 +0100

    Update PE_extraction.py

--- PE_extraction.py
+++ PE_extraction.py
@@ -18,15 +18,6 @@ debut = time.time()
 API_KEY = os.environ.get('api_key')
 API_ID_CLIENT = os.environ.get('api_id_client')
 
-API_c1 = "PAR_dat"
-API_c2 = "ascientest_7cc80d476c"
-API_c3 = "bbaaa1b635864f02d8825a4e46eee4d87e45bc7e6642770741fa6f"
-API_ID_CLIENT = API_c1+API_c2+API_c3
-API_k1 = "e888aeb08dd9e4a022"
-API_k2 = "d7895b851f305c6d343692"
-API_k3 = "b0b52ef8135f1418a3661bc0"
-API_KEY = API_k1+API_k2+API_k3
-
 print("***********", API_KEY, "*****************")
 print("***********", API_ID_CLIENT, "*****************")
 
